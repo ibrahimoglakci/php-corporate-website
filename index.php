0bf867ce10336a30e67ed8acaba44ef070e12dd5
Ibrahim Oglakci <contact@ibrahimoglakci.com>
2023-10-10 12:16:26 +0300

    Created Corporate Website

---
+++ index.php
@@ -0,0 +1,310 @@
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta charset="utf-8">
+    <title>Kurumsal Web Site</title>
+
+
+    <!-- Fontlar -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800|Montserrat:300,400,700"
+        rel="stylesheet">
+
+    <!-- Bootstrap stil dosyası -->
+    <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- işimize yarayacak diğer kütüphane css dosyalarımız -->
+    <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/magnific-popup/magnific-popup.css" rel="stylesheet">
+    <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+
+    <!-- bizim stil dosyamız -->
+    <link href="css/style.css" rel="stylesheet">
+
+
+</head>
+
+<body id="body">
+
+    <!-- ÜstBar-->
+
+    <section id="topbar" class="d-none d-lg-block">
+
+        <div class="container clearfix">
+
+            <div class="contact-info float-left">
+
+                <i class="fa fa-envelope-o"></i><a href="mailto:info@teamplayz.com">info@teamsoftdevs.com</a>
+                <i class="fa fa-phone"></i>+90 538 029 3758
+            </div>
+            <div class="social-links float-right">
+                <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+                <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+                <a href="#" class="youtube"><i class="fa fa-youtube"></i></a>
+                <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+            </div>
+        </div>
+
+
+
+    </section>
+
+
+
+    <!-- Header-->
+
+    <header id="header">
+
+        <div class="container">
+            <div id="logo" class="pull-left">
+                <h1><a href="#body" class="scrollto"> TeamSoft <span>Development</span></a></h1>
+            </div>
+
+            <nav id="nav-menu-container">
+                <ul class="nav-menu">
+                    <li class="menu-active"><a href="#body">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#shop">Shop</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#comments">Comments</a></li>
+                    <li><a href="#contact">Contact</a></li>
+
+                </ul>
+            </nav>
+
+        </div>
+
+
+    </header>
+
+    <!-- Intro -->
+
+    <section id="intro">
+
+        <div class="intro-content">
+            <h2>We are coding with safely</h2>
+        </div>
+
+        <div id="intro-carousel" class="owl-carousel">
+
+
+            <div class="item" style="background-image:url('img/carousel/1.jpg');"></div>
+            <div class="item" style="background-image:url('img/carousel/2.jpg');"></div>
+            <div class="item" style="background-image:url('img/carousel/3.jpg');"></div>
+            <div class="item" style="background-image:url('img/carousel/4.jpg');"></div>
+            <div class="item" style="background-image:url('img/carousel/5.jpg');"></div>
+
+
+        </div>
+
+    </section>
+
+
+    <!-- Main -->
+
+    <section id="about" class="wow fadeInUp">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 about-img">
+                    <img src="img/about.jpg" alt="" />
+                </div>
+
+                <div class="col-lg-6 content">
+                    <h2>Lorem ipsum Lorem ipsum</h2>
+
+                    <h3>Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
+                        ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum</h3>
+
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+
+
+    <section id="services">
+        <div class="container">
+            <div class="section-header">
+                <h2>Our Services</h2>
+                <p>We are so succesfully of coding bla bla bla bla bla</p>
+            </div>
+            <div class="row">
+
+                <div class="col-lg-6">
+                    <div class="box wow fadeInLeft">
+                        <div class="icon"><i class="fa fa-bar-chart"></i></div>
+                        <h4 class="title"><a href="#">Lorem Ipsum</a></h4>
+                        <p class="description">bla bla bla bla bla bla bla lbal lasfl asdl lasd lv bla lba</p>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="box wow fadeInLeft">
+                        <div class="icon"><i class="fa fa-bar-chart"></i></div>
+                        <h4 class="title"><a href="#">Lorem Ipsum</a></h4>
+                        <p class="description">bla bla bla bla bla bla bla lbal lasfl asdl lasd lv bla lba</p>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="box wow fadeInLeft">
+                        <div class="icon"><i class="fa fa-bar-chart"></i></div>
+                        <h4 class="title"><a href="#">Lorem Ipsum</a></h4>
+                        <p class="description">bla bla bla bla bla bla bla lbal lasfl asdl lasd lv bla lba</p>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="box wow fadeInLeft">
+                        <div class="icon"><i class="fa fa-bar-chart"></i></div>
+                        <h4 class="title"><a href="#">Lorem Ipsum</a></h4>
+                        <p class="description">bla bla bla bla bla bla bla lbal lasfl asdl lasd lv bla lba</p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+
+
+    <!-- References -->
+
+    <section id="references" class="wow fadeInUp">
+        <div class="container">
+            <div class="section-header">
+                <h2>References</h2>
+                <p>asşldjkwqldjqwldmazscmalknaslkdnaslkdnqwlknasdjalskdjaslkdn askldnsaldknsadlknsa</p>
+            </div>
+
+            <div class="owl-carousel clients-carousel">
+                <img src="img/referans/ref1.png" alt="" />
+                <img src="img/referans/ref2.png" alt="" />
+                <img src="img/referans/ref3.png" alt="" />
+                <img src="img/referans/ref4.png" alt="" />
+                <img src="img/referans/ref5.png" alt="" />
+                <img src="img/referans/ref6.png" alt="" />
+                <img src="img/referans/ref7.png" alt="" />
+                <img src="img/referans/ref8.png" alt="" />
+            </div>
+        </div>
+
+    </section>
+
+
+    <!-- Galleries -->
+
+    <section id="galleries" class="wow fadeInUp">
+        <div class="container">
+            <div class="section-header">
+                <h2>Our Example Projects</h2>
+                <p>asşldjkwqldjqwldmazscmalknaslkdnaslkdnqwlknasdjalskdjaslkdn askldnsaldknsadlknsa</p>
+            </div>
+        </div>
+        <div class="container-fluid">
+
+            <div class="row no-gutters">
+
+                <div class="col-lg-3 col-md-4">
+                    <div class="galleries-item wow fadeInUp">
+                        <a href="#galleries" class="galleries-popup"><img src="img/filo/1.jpg" alt="" />
+                            <div class="galleries-overlay">
+
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4">
+                    <div class="galleries-item wow fadeInUp">
+                        <a href="#galleries" class="galleries-popup"><img src="img/filo/2.jpg" alt="" />
+                            <div class="galleries-overlay">
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4">
+                    <div class="galleries-item wow fadeInUp">
+                        <a href="#galleries" class="galleries-popup"><img src="img/filo/3.jpg" alt="" />
+                            <div class="galleries-overlay">
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4">
+                    <div class="galleries-item wow fadeInUp">
+                        <a href="#galleries" class="galleries-popup"><img src="img/filo/4.jpg" alt="" />
+                            <div class="galleries-overlay">
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4">
+                    <div class="galleries-item wow fadeInUp">
+                        <a href="#galleries" class="galleries-popup"><img src="img/filo/5.jpg" alt="" />
+                            <div class="galleries-overlay">
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4">
+                    <div class="galleries-item wow fadeInUp">
+                        <a href="#galleries" class="galleries-popup"><img src="img/filo/6.jpg" alt="" />
+                            <div class="galleries-overlay">
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4">
+                    <div class="galleries-item wow fadeInUp">
+                        <a href="#galleries" class="galleries-popup"><img src="img/filo/7.jpg" alt="" />
+                            <div class="galleries-overlay">
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4">
+                    <div class="galleries-item wow fadeInUp">
+                        <a href="#galleries" class="galleries-popup"><img src="img/filo/8.jpg" alt="" />
+                            <div class="galleries-overlay">
+
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+
+
+
+            </div>
+        </div>
+
+    </section>
+
+
+
+    <!-- Libraries -->
+    <script src="lib/jquery/jquery.min.js"></script>
+    <script src="lib/jquery/jquery-migrate.min.js"></script>
+    <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/superfish/hoverIntent.js"></script>
+    <script src="lib/superfish/superfish.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/magnific-popup/magnific-popup.min.js"></script>
+    <script src="lib/sticky/sticky.js"></script>
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
